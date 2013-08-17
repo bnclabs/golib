f88ba3b43017dd43cf0da5a0ae7c2a3c55cb12de
prataprc <prataprc@gmail.com>
2013-08-17 16:49:08 +0530

    Added function to readlines from file.

---
+++ fileio.go
@@ -0,0 +1,13 @@
+package golib
+
+func Readlines( filename string ) []string {
+    var lines = make( []string, 0 )
+    fd, _ := os.Open(filename)
+    defer func(){ fd.Close() }()
+    scanner := bufio.NewScanner(fd)
+    for scanner.Scan() {
+        lines = append( lines, string(scanner.Bytes()) )
+    }
+    return lines
+}
+
